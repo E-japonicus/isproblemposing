749a25d3f22879d69c5720c582cfbecfb56282ef
Masaki Sato <mustang.97ms@gmail.com>
2019-04-17 10:02:47 +0900

    first commit

---
+++ lang/en/isproblemposing.php
@@ -0,0 +1,42 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * English strings for isproblemposing
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package    mod_isproblemposing
+ * @copyright  2016 Your Name <your@email.address>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'isproblemposing';
+$string['modulenameplural'] = 'isproblemposings';
+$string['modulename_help'] = 'Use the isproblemposing module for... | The isproblemposing module allows...';
+$string['isproblemposing:addinstance'] = 'Add a new isproblemposing';
+$string['isproblemposing:submit'] = 'Submit isproblemposing';
+$string['isproblemposing:view'] = 'View isproblemposing';
+$string['isproblemposingfieldset'] = 'Custom example fieldset';
+$string['isproblemposingname'] = 'isproblemposing name';
+$string['isproblemposingname_help'] = 'This is the content of the help tooltip associated with the isproblemposingname field. Markdown syntax is supported.';
+$string['isproblemposing'] = 'isproblemposing';
+$string['pluginadministration'] = 'isproblemposing administration';
+$string['pluginname'] = 'isproblemposing';
